e6c7f77573cd6602b76cfefed948640af68a27d0
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2021-11-09 21:09:25 +0100

    sandbox created

---
+++ src/components/Card.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">My first card</h5>
+            <p class="card-text">Description of the card</p>
+            <a href="#" class="btn btn-primary">My button</a>
+        </div>
+    </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Hello world!</h1>
+    <card/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from "../components/Card.vue"
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
